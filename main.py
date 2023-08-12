020b18dcdf3c9b0d3ba42df665c8b931f49e5db2
Aisya-A5 <121149189+Aisya-A5@users.noreply.github.com>
2023-08-12 10:49:44 +0700

    2main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            "ROFL" : "tanggapan terhadap lelucon",
-            "SHEESH" : "sedikit ketidaksetujuan",
-            "CREEPY" : "menakutkan, tidak menyenangkan",
-            "AGGRO" : "untuk menjadi agresif/marah",
-            }
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+import random
 
-if word in meme_dict.keys():
-    # Apa yang harus kita lakukan jika kata itu ditemukan?
-    print(meme_dict[word])
-else:
-    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
-    print('not found 404')
+keyword = '+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+password = ''
+
+pw_length = int(input('masukan panjang password'))
+
+for i  in range(pw_length):
+    password += random.choice(keyword)
+
+print(password)
