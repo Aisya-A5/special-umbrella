565eafd66076a331e466fa04930489dfd4433701
Aisya-A5 <121149189+Aisya-A5@users.noreply.github.com>
2023-08-26 10:59:21 +0700

    Update_main.py

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
-import random
+import discord
+from bot_logic import gen_pass, coin_sides, gen_emodji
+from discord.ext import commands
 
-keyword = '+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-password = ''
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
-pw_length = int(input('masukan panjang password'))
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
 
-for i  in range(pw_length):
-    password += random.choice(keyword)
+@bot.command()
+async def bye(ctx):
+    await ctx.send("\U0001f642")
 
-print(password)
+@bot.command()
+async def pwd(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def koin(ctx):
+    await ctx.send(coin_sides())
+
+
+@bot.command()
+async def react(ctx):
+    await ctx.send(gen_emodji())
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def deleteme(ctx):
+    await ctx.send("I will delete it in 3 seconds...", delete_after=3.0)
+
+async def on_message_delete(self, ctx):
+        ctx = f'{ctx.author} has deleted the message: {ctx.content}'
+        await ctx.channel.send(ctx)
+
+bot.run("your bot token")
